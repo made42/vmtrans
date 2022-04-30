bec90ad75af8819d941fffd4291bcaf8f51fe9bd
Maarten Derks <made@freedom.nl>
2022-04-30 12:00:37 +0200

    Implement and

--- CodeWriter.java
+++ CodeWriter.java
@@ -131,6 +131,14 @@ class CodeWriter {
                 printWriter.println("(ltcont" + ltcounter + ")");
                 ltcounter++;
                 break;
+            case "and": // x and y
+                printWriter.println("@SP");
+                printWriter.println("AM=M-1");
+                printWriter.println("D=M");
+                printWriter.println("@SP");
+                printWriter.println("A=M-1");
+                printWriter.println("M=M&D");
+                break;
             default:
                 write(Parser.currentCommand);
                 break;
